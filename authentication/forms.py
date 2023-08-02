0896c6ee743530f8c72b9bf4912b1cb9d78b3a34
rahuldhamecha <rahul.dhamecha@infobeans.com>
2023-08-03 00:44:54 +0530

    First  name

--- authentication/forms.py
+++ authentication/forms.py
@@ -12,7 +12,7 @@ class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta():
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password','first_name','last_name')
 
 class LoginForm(forms.Form):
     username = forms.CharField(required=True)
